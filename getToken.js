593e8b0ef5d41d604a668e8f30814b22153e90fd
root <root@corrupted>
2024-10-14 10:03:28 +0530

    Token mechanism

---
+++ getToken.js
@@ -0,0 +1,73 @@
+const fetch = require('node-fetch');
+require('dotenv').config();
+//three parts for getting tokens
+//To avail a fresh new set of access_token and refresh_token 
+
+
+
+
+
+
+//First Step
+//call this url from a broswer placing proper client_id and redirect_uri ,redirect_uri needs to be whitelisted in eagle eye dashboard console for our user. 
+
+
+
+
+var s = `https://auth.eagleeyenetworks.com/oauth2/authorize?scope=vms.all&client_id=${process.env.CLIENT_ID}&response_type=code&redirect_uri=http://localhost:3333/`  
+
+console.log(s);
+
+
+// call this function to request access_token and refresh_token after getting a token.
+const getTokens = async (code) => {
+    let endpoint = 'https://auth.eagleeyenetworks.com/oauth2/token'
+    let params = new URLSearchParams({
+        grant_type: "authorization_code",
+        code: code,
+        scope: "vms.all",
+        redirect_uri: `http://${hostName}:${port}`
+    });
+
+    const url = `${endpoint}?${params.toString()}`;
+    try {
+        const response = await axios.post(url, {}, {       
+            auth: {
+                username: clientId,
+                password: clientSecret
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
+
+// This function will request a new access_token using the refresh token.
+const refreshToken = async (refresh_token) => {
+    let endpoint = 'https://auth.eagleeyenetworks.com/oauth2/token'
+    let params = new URLSearchParams({
+        grant_type: "refresh_token",
+        refresh_token: refresh_token,
+        scope: "vms.all"
+    });
+    const url = `${endpoint}?${params.toString()}`;
+
+    try {
+        const response = await axios.post(url, {}, {
+            auth: {
+                username: clientId,
+                password: clientSecret
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.log(error, "refresh token error");
+        throw error;
+    }
+};
+
+
+
+
